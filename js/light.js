f5f9071a158fa443dd2edc56c24d46eab308d88d
GuillaumeJulien <Guillaume.Julien@etu.univ-savoie.fr>
2016-06-19 11:53:43 +0200

    pulse Light

--- js/light.js
+++ js/light.js
@@ -6,26 +6,57 @@ Light.prototype.init = function () {
     this.initEvents();
 };
 Light.prototype.initVars = function () {
+    this.toggled = false;
     this.domElement = document.getElementById('light-'+this.id);
+    this.popinEl= document.getElementById('popin-tuto');
     this.x = this.domElement.offsetLeft;
     this.y = this.domElement.offsetTop;
+
 };
 Light.prototype.initEvents = function () {
-   var self = this;
 };
+
 Light.prototype.setActive = function () {
   var self = this;
-  if (self.domElement.classList){
-    self.domElement.classList.add("active");
-  }else{
-    self.domElement.className += ' ' + 'active';
+  if(!self.toggled){
+    if (self.domElement.classList){
+      self.domElement.classList.add("active");
+    }else{
+      self.domElement.className += ' ' + 'active';
+    }
+    self.setActivePopin();
+    setTimeout(self.setInactivePopin.bind(this), 3000);
   }
+  setTimeout(function () {
+    self.toggled = false;
+  }, 5000);
+  return self;
 };
+
 Light.prototype.setInactive = function () {
   self =this;
   if (self.domElement.classList){
     self.domElement.classList.remove("active");
   }else{
     self.domElement.className = self.domElement.className.replace(new RegExp('(^|\\b)' + "active".split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+  }
+};
+
+Light.prototype.setActivePopin = function () {
+  var self = this;
+  if (self.popinEl.classList){
+    self.popinEl.classList.add("active");
+  }else{
+    self.popinEl.className += ' ' + 'active';
+  }
+};
+
+Light.prototype.setInactivePopin = function () {
+  var self = this;
+  self.toggled = true;
+  if (self.popinEl.classList){
+    self.popinEl.classList.remove("active");
+  }else{
+    self.popinEl.className = self.popinEl.className.replace(new RegExp('(^|\\b)' + "active".split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
   }
 };
--- js/viseur.js
+++ js/viseur.js
@@ -11,7 +11,10 @@ Viseur.prototype.init = function () {
 Viseur.prototype.initVars = function () {
     self = this;
     this.viseurel = document.getElementById('viseur');
-    this.domElementPopin = document.getElementById('popin-tuto')
+    this.domElementPopin = document.getElementById('popin-tuto');
+    this.innerCircle = document.getElementById('inner-circle');
+    this.outterCircle = document.getElementById('outter-circle');
+    this.activeLight = {};
     var numOfLight = document.getElementsByClassName('light').length;
     for (var i=1; i < numOfLight; i++ ) {
         var light = new Light(i);
@@ -32,13 +35,47 @@ Viseur.prototype.initEvents = function () {
     self.x =  self.viseurel.offsetLeft;
     self.y = self.viseurel.offsetTop;
     for(var light in lights){
-      if (isAround(lights[light],self)<= 85) {
-        lights[light].setActive();
+      if (isAround(lights[light],self)<= 120) {
+        self.activeLight = lights[light].setActive();
       }else {
         lights[light].setInactive();
+
       }
     }
   };
+  document.addEventListener('mousedown', self.setActive.bind(this), true);
+  document.addEventListener('mouseup', self.setInactive.bind(this), true);
+};
+
+Viseur.prototype.setActive = function () {
+  var self = this;
+  if (self.innerCircle.classList && self.outterCircle.classList){
+    self.innerCircle.classList.add("active");
+    self.outterCircle.classList.add("active");
+  }else{
+    self.innerCircle.className += ' ' + 'active';
+    self.outterCircle.className += ' ' + 'active';
+  }
+  if(self.activeLight.domElement.classList){
+    self.activeLight.domElement.classList.add("analize");
+  }else{
+      self.activeLight.domElement.className += ' ' + 'analize';
+  }
+};
+Viseur.prototype.setInactive = function () {
+  var self = this;
+  if (self.innerCircle.classList && self.outterCircle.classList.remove ){
+    self.innerCircle.classList.remove("active");
+    self.outterCircle.classList.remove("active");
+  }else{
+    self.innerCircle.className = self.viseurel.className.replace(new RegExp('(^|\\b)' + "active".split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+    self.outterCircle.className = self.viseurel.className.replace(new RegExp('(^|\\b)' + "active".split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+  }
+  if(self.activeLight.domElement.classList){
+    self.activeLight.domElement.classList.remove("analize");
+  }else{
+    self.activeLight.domElement.className =  self.domElement.className.replace(new RegExp('(^|\\b)' + "active".split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+  }
 };
 
 viseur = new Viseur();
